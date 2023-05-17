ed0abbda1703ae9add99b1aa5da198296e8fa3cd
eabz <eabz@kindynos.mx>
2023-05-16 18:11:29 -0600

    Finish

--- README.md
+++ README.md
@@ -1,13 +1,43 @@
 # Llama Auctions Subgraph
 
-1. Install the graph cli
-2. Run command
+Subgraph to track Llamas Auctions House https://thellamas.io
 
-graph init @llama/auctions-subgraph ./subpgrah \
- --index-events \
- --protocol ethereum \
- --product hosted-service \
- --from-contract 0x47a37ea6b691eb76aaf771d5bdbba2ca68add173 \
- --network mainnet \
- --contract-name LlamaAuctionHouse \
- --start-block 17145207
+## Getting Started
+
+First of all, install the dependencies to run this app:
+
+- [NodeJS](https://nodejs.org)
+- [pnpm](https://pnpm.io/installation)
+
+```bash
+# Install the graph cli globally
+$ npm i -g @graphprotocol/graph-cli
+
+# Clone this repository
+$ git clone https://github.com/eabz/llama-auctions-subgraph && cd llama-auctions-subgraph
+
+# Install dependencies
+$ pnpm i
+
+# Generate the code
+$ pnpm codegen
+
+# Build the graph to upload
+$ pnpm build
+
+# Lints and fixes files
+$ pnpm lint
+```
+
+## Commands
+
+- `codegen`: generates the graph specific structures from the abi.
+- `build`: compile the subpgrah code.
+- `deploy`: deploy the subgraph to the hosted service.
+- `lint`: runs the linter in all the files.
+
+Check [package.json](./package.json) for other useful scripts.
+
+## Contribution Guidelines
+
+Developers are expected to follow contribution guidelines to keep the codebase efficient, readable, and standardized. Contribution guidelines are clearly laid out so developers and contributors can submit their work without much revision, resulting in faster development and more useful contributions. These guideliness are specified through the `rome` configuration.
--- src/llama-auction-house.ts
+++ src/llama-auction-house.ts
@@ -1,4 +1,3 @@
-import { Address, BigInt, ByteArray, Bytes } from '@graphprotocol/graph-ts'
 import {
 	AuctionBid as AuctionBidEvent,
 	AuctionCreated as AuctionCreatedEvent,
@@ -8,136 +7,148 @@ import {
 	AuctionReservePriceUpdated as AuctionReservePriceUpdatedEvent,
 	AuctionSettled as AuctionSettledEvent,
 	AuctionTimeBufferUpdated as AuctionTimeBufferUpdatedEvent,
-	Withdraw as WithdrawEvent
-} from '../generated/LlamaAuctionHouse/LlamaAuctionHouse'
+	Withdraw as WithdrawEvent,
+} from "../generated/LlamaAuctionHouse/LlamaAuctionHouse";
 import {
 	Auction,
 	AuctionGlobalConfiguration,
-	CurrentAuction
-} from '../generated/schema'
+	CurrentAuction,
+} from "../generated/schema";
+import { Address, BigInt, ByteArray, Bytes } from "@graphprotocol/graph-ts";
 
-const mainConfigId = 'LlamaAuctionHouseConfig'
-const currentAuctionId = 'LlamaAuctionHouseCurrentAuction'
+const mainConfigId = "LlamaAuctionHouseConfig";
+const currentAuctionId = "LlamaAuctionHouseCurrentAuction";
 
 export function handleAuctionBid(event: AuctionBidEvent): void {
-	const id = Bytes.fromUTF8(currentAuctionId)
+	const id = Bytes.fromUTF8(currentAuctionId);
 
-	let entity = CurrentAuction.load(id)
+	let entity = CurrentAuction.load(id);
 	if (!entity) {
-		entity = new CurrentAuction(id)
+		entity = new CurrentAuction(id);
 	}
 
-	entity.latest_bid_amount = event.params._value
-	entity.latest_bidder = event.params._sender
-	entity.total_bids = entity.total_bids.plus(BigInt.fromI64(1))
+	entity.winning_bid = event.params._value;
+	entity.winner = event.params._sender;
+	entity.total_bids = entity.total_bids.plus(BigInt.fromI64(1));
 
-	entity.save()
+	entity.save();
 }
 
 export function handleAuctionExtended(event: AuctionExtendedEvent): void {
-	const id = Bytes.fromUTF8(currentAuctionId)
+	const id = Bytes.fromUTF8(currentAuctionId);
 
-	let entity = CurrentAuction.load(id)
+	let entity = CurrentAuction.load(id);
 	if (!entity) {
-		entity = new CurrentAuction(id)
+		entity = new CurrentAuction(id);
 	}
 
-	entity.end_time = event.params._end_time
+	entity.end_time = event.params._end_time;
 
-	entity.save()
+	entity.save();
 }
 
 export function handleAuctionCreated(event: AuctionCreatedEvent): void {
-	const id = Bytes.fromUTF8(currentAuctionId)
+	const id = Bytes.fromUTF8(currentAuctionId);
 
-	let entity = CurrentAuction.load(id)
+	let entity = CurrentAuction.load(id);
 	if (!entity) {
-		entity = new CurrentAuction(id)
+		entity = new CurrentAuction(id);
 	}
 
-	entity.llama_id = event.params._llama_id
-	entity.end_time = event.params._end_time
-	entity.start_time = event.params._start_time
-	entity.total_bids = BigInt.fromI64(0)
-	entity.latest_bid_amount = BigInt.fromI64(0)
-	entity.latest_bidder = Address.zero()
+	entity.llama_id = event.params._llama_id;
+	entity.end_time = event.params._end_time;
+	entity.start_time = event.params._start_time;
+	entity.total_bids = BigInt.fromI64(0);
+	entity.winning_bid = BigInt.fromI64(0);
+	entity.winner = Address.zero();
 
-	entity.save()
+	entity.save();
 }
 
 export function handleAuctionSettled(event: AuctionSettledEvent): void {
-	const id = Bytes.fromUTF8(currentAuctionId)
+	const id = Bytes.fromUTF8(currentAuctionId);
 
-	const entity = CurrentAuction.load(id)
-	if (!entity) return
+	const entity = CurrentAuction.load(id);
+	if (!entity) return;
 
-	const auctionId = Bytes.fromByteArray(ByteArray.fromBigInt(entity.llama_id))
+	const auctionId = Bytes.fromByteArray(ByteArray.fromBigInt(entity.llama_id));
 
-	const auction = new Auction(auctionId)
+	const auction = new Auction(auctionId);
 
-	auction.llama_id = entity.llama_id
-	auction.start_time = entity.start_time
-	auction.end_time = entity.end_time
-	auction.total_bids = entity.total_bids
-	auction.latest_bid_amount = entity.latest_bid_amount
-	auction.latest_bidder = entity.latest_bidder
+	auction.llama_id = entity.llama_id;
+	auction.start_time = entity.start_time;
+	auction.end_time = entity.end_time;
+	auction.total_bids = entity.total_bids;
+	auction.amount = event.params._amount;
+	auction.winner = event.params._winner;
 
-	auction.save()
+	auction.save();
 }
 
 export function handleAuctionTimeBufferUpdated(
-	event: AuctionTimeBufferUpdatedEvent
+	event: AuctionTimeBufferUpdatedEvent,
 ): void {
-	const id = Bytes.fromUTF8(mainConfigId)
+	const id = Bytes.fromUTF8(mainConfigId);
 
-	let entity = AuctionGlobalConfiguration.load(id)
+	let entity = AuctionGlobalConfiguration.load(id);
 	if (!entity) {
-		entity = new AuctionGlobalConfiguration(id)
+		entity = new AuctionGlobalConfiguration(id);
+		setDefaultGlobalConfig(entity);
 	}
 
-	entity.time_buffer = event.params._time_buffer
-	entity.save()
+	entity.time_buffer = event.params._time_buffer;
+	entity.save();
 }
 
 export function handleAuctionMinBidIncrementPercentageUpdated(
-	event: AuctionMinBidIncrementPercentageUpdatedEvent
+	event: AuctionMinBidIncrementPercentageUpdatedEvent,
 ): void {
-	const id = Bytes.fromUTF8(mainConfigId)
+	const id = Bytes.fromUTF8(mainConfigId);
 
-	let entity = AuctionGlobalConfiguration.load(id)
+	let entity = AuctionGlobalConfiguration.load(id);
 	if (!entity) {
-		entity = new AuctionGlobalConfiguration(id)
+		entity = new AuctionGlobalConfiguration(id);
+		setDefaultGlobalConfig(entity);
 	}
 
 	entity.min_bid_increment_percentage =
-		event.params._min_bid_increment_percentage
-	entity.save()
+		event.params._min_bid_increment_percentage;
+	entity.save();
 }
 
 export function handleAuctionReservePriceUpdated(
-	event: AuctionReservePriceUpdatedEvent
+	event: AuctionReservePriceUpdatedEvent,
 ): void {
-	const id = Bytes.fromUTF8(mainConfigId)
+	const id = Bytes.fromUTF8(mainConfigId);
 
-	let entity = AuctionGlobalConfiguration.load(id)
+	let entity = AuctionGlobalConfiguration.load(id);
 	if (!entity) {
-		entity = new AuctionGlobalConfiguration(id)
+		entity = new AuctionGlobalConfiguration(id);
+		setDefaultGlobalConfig(entity);
 	}
 
-	entity.reserve_price = event.params._reserve_price
-	entity.save()
+	entity.reserve_price = event.params._reserve_price;
+	entity.save();
 }
 
 export function handleAuctionDurationUpdated(
-	event: AuctionDurationUpdatedEvent
+	event: AuctionDurationUpdatedEvent,
 ): void {
-	const id = Bytes.fromUTF8(mainConfigId)
+	const id = Bytes.fromUTF8(mainConfigId);
 
-	let entity = AuctionGlobalConfiguration.load(id)
+	let entity = AuctionGlobalConfiguration.load(id);
 	if (!entity) {
-		entity = new AuctionGlobalConfiguration(id)
+		entity = new AuctionGlobalConfiguration(id);
+		setDefaultGlobalConfig(entity);
 	}
 
-	entity.duration = event.params._duration
-	entity.save()
+	entity.duration = event.params._duration;
+	entity.save();
+}
+
+function setDefaultGlobalConfig(entity: AuctionGlobalConfiguration): void {
+	entity.time_buffer = BigInt.fromI64(300);
+	entity.reserve_price = BigInt.fromString("200000000000000000");
+	entity.min_bid_increment_percentage = BigInt.fromI64(2);
+	entity.duration = BigInt.fromI64(5400);
 }
